33865e398b2b605d73e0627a088a9f22f4165218
Maks <115720445+MaksHromi@users.noreply.github.com>
2023-06-26 12:16:01 +0300

    Update Kolokwium1EFZadA.py

--- Kolokwium1EFZadA.py
+++ Kolokwium1EFZadA.py
@@ -1,11 +1,11 @@
-O = 0
+Najmniejsza_liczba = 0
 
 liczba = int(input("wpisz liczbe - "))
 
-while liczba != :O
-    if liczba > O:
-         = liczba
+while liczba != 0:
+    if liczba > Najmniejsza_liczba:
+        Najmniejsza_liczba = liczba
     print(liczba)
     liczba = int(input("wpisz liczbe: "))
 
-print("Najmniejsza liczba to -", 0)
+print("Najmniejsza liczba to -", Najmniejsza_liczba)
