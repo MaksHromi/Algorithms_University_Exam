b441b164dd8edecd6b98cf966e5c8b0ac61b896e
Maks <115720445+MaksHromi@users.noreply.github.com>
2023-06-26 12:14:43 +0300

    Add files via upload

---
+++ Kolokwium1EFZadA.py
@@ -0,0 +1,11 @@
+O = 0
+
+liczba = int(input("wpisz liczbe - "))
+
+while liczba != :O
+    if liczba > O:
+         = liczba
+    print(liczba)
+    liczba = int(input("wpisz liczbe: "))
+
+print("Najmniejsza liczba to -", 0)
